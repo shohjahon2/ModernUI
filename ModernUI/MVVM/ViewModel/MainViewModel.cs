bf7486d2e091af9bdad57d8670307bf4f3ed1610
aliabdirakhmonov <aliabdirakhmonov728@gmail.com>
2024-02-29 15:51:31 +0500

    Add project files.

---
+++ ModernUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using ModernUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernUI.MVVM.ViewModel
+{
+    public class MainViewModel: ObresvableObject
+    {
+        public RelayCommand HomewViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        private HomeViewModel HomeVM { get; set; }
+        private DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+
+            HomewViewCommand = new RelayCommand(o=>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o=>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
